5fa015a4ebd8f059324f0a4f293128eb47de5aee
Axelfoley <axel.roemer@posteo.de>
2022-08-02 12:39:48 +0200

    use docker-compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-cd /captain-hook/
-npm test
+echo ${ENVIRONMENT}
+if [[ "${ENVIRONMENT}" == "testing" ]]; then
+    echo "Running tests"
+    cd /captain-hook/
+    npm test
+elif [[ "${ENVIRONMENT}" == "production" ]]; then
+    echo "Running production"
+    cd /captain-hook/
+    node captainHook.js
+else
+    echo "ENVIRONMENT was unknown, staying alive"
+    tail -f /dev/null
+fi
