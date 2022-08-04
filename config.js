331f7c2a71f8c8c1f58ce603e4212ab947971a46
Axelfoley <axel.roemer@posteo.de>
2022-08-04 15:24:04 +0200

    revert unsuccessfull changes

--- config.js
+++ config.js
@@ -17,8 +17,8 @@ module.exports = {
     production: {
         username: process.env.username || null,
         password: process.env.password || null,
-        database: process.env.database || 'data/test.sqlite',
-        host: process.env.host || 'data/test.sqlite',
+        database: process.env.database || 'test.sqlite',
+        host: process.env.host || 'test.sqlite',
         dialect: process.env.dialect || 'sqlite'
     }
 }
