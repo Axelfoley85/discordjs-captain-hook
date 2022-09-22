e938952a6685ce8ff4db8d4f59f817b08fbf55a5
Axelfoley <axel.roemer@posteo.de>
2022-09-22 12:36:00 +0200

    add ping for volunteering poll

--- valueObjects/scheduledPolls.js
+++ valueObjects/scheduledPolls.js
@@ -2,13 +2,14 @@ const {
     attendanceChannel,
     adminChannel
 } = require('../config')
+const { everyone } = require('./roles')
 
 module.exports = {
     scheduledPolls: [
         {
             channel: adminChannel,
             cron: '0 8 * * 1',
-            title: 'Anybody volunteering to',
+            title: `<@&${everyone.id}> Anybody volunteering to`,
             options: [
                 'set up the donation glass, for the rent etc., at the door ' +
                 'next Wednesday and putting it onto the kitchen table at ' +
