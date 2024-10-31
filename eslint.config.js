748037a7c80b1e7fd3391a7faf9430da1ccace71
Axel Römer <22745501+Axelfoley85@users.noreply.github.com>
2024-10-31 20:41:04 +0100

    replace unmaintained eslint standard with neostandard

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,22 @@
 import globals from 'globals'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-import js from '@eslint/js'
-import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-})
-
-export default [...compat.extends('standard'), {
-    languageOptions: {
-        globals: {
-            ...globals.browser
-        },
-
-        ecmaVersion: 'latest',
-        sourceType: 'module'
+export default [
+    {
+        ignores: ['coverage/**/*.js', 'test/**/*.js'],
     },
-
-    rules: {
-        indent: ['error', 4],
-        'max-len': ['error', 80]
+    {
+        files: ['**/*.js'],
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            globals: {
+                ...globals.browser,
+                ...globals.node
+            },
+        },
+        rules: {
+            'indent': ['error', 4],
+            'max-len': ['error', { code: 80 }],
+        }
     }
-}]
+]
